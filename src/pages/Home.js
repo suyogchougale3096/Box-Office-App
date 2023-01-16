39350e8a72f3c66159531dd161a872a0334c0299
Suyog <109020086+suyogchougale3096@users.noreply.github.com>
2023-01-16 22:45:15 +0530

    16-01-2023

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
   let [input, setInput] = useState("");
+  let [results, setResults] = useState(null);
 
   const onInputChange = (ev) => {
     setInput(ev.target.value);
@@ -11,17 +13,38 @@ const Home = () => {
 
   // https://api.tvmaze.com/singlesearch/shows?q=mens
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((result) => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+      // console.log(result);
+    });
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then((r) => r.json())
+    //   .then((result) => {
+    //     setResults(result);
+    //     console.log(result);
+    //   });
   };
 
   const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results.</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
   return (
     <div>
@@ -35,6 +58,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </div>
   );
 };
