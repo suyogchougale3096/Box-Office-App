67142dd07dd15eed7989c49ef0ef6712a3784fc8
Suyog <109020086+suyogchougale3096@users.noreply.github.com>
2023-01-18 17:04:02 +0530

    18-01-2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import Show from "./pages/Show";
 import Starred from "./pages/Starred";
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/starred" element={<Starred />} />
+          <Route exact path = "/show/:id" element = {<Show/>}/>
         </Routes>
       </div>
     );
